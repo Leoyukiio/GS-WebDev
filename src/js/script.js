6731ddd4ac0964be7df1389dd790510e7b3f71bd
Léo Yukio <leoyukio06@gmail.com>
2025-06-06 16:23:28 -0300

    Criação do mudar fundo

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,14 @@
 function toggleMenu() {
-    const navList = document.getElementById("nav-list");
-    navList.style.display = navList.style.display === "flex" ? "none" : "flex";
+  const navList = document.getElementById("nav-list");
+  navList.style.display = navList.style.display === "flex" ? "none" : "flex";
+}
+
+function mudarFundo(estilo) {
+  if (estilo === "claro") {
+    document.body.style.backgroundColor = "#f0f0f0";
+  } else if (estilo === "escuro") {
+    document.body.style.backgroundColor = "#222";
+  } else {
+    document.body.style.backgroundColor = "#ffffff";
+  }
 }
